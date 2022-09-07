039c7d580ef86b496f796b89029f7a1fb97f1f8e
Klaudiuszu <klaudiuszpupiec.kontakt@gmail.com>
2022-09-07 20:42:25 +0200

    create pages with react router

---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useEffect, useMemo, useState } from "react";
+import axios from "axios";
+
+function Login() {
+
+    return (
+        <div>dumba1</div>
+    )
+
+}
+
+export default Login;
