a468cb2377c9beaa663be63baa1c8c537d6816a3
Klaudiuszu <klaudiuszpupiec.kontakt@gmail.com>
2022-09-08 23:23:26 +0200

    finish Login css

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,37 +2,76 @@ import React from "react";
 import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
+import { images } from "../../utils";
+
 import "./Login.scss";
 
 function Login() {
+  const [btnState, setBtnState] = useState(false);
+
+  function handleClick() {
+    setBtnState((btnState) => !btnState);
+  }
+
+  let toggleCardContainer = btnState ? " app__Login-card-container" : null;
+
   return (
-    <div className="app__Login">
+    <>
       <div className="container"></div>
-      <div className="app__Login-card"></div>
-      <button
-        type="button"
-        className="modal-button"
-        data-toggle="modal"
-        data-target="#exampleModal"
+      <div
+        className="app__Login modal"
+        style={{
+          height: btnState ? "100vh" : "60px",
+        }}
       >
-        Click here to login
-      </button>
-      {/* <div className="app__Login-wrapper">
-        <form>
-          <label>
-            <p>Username</p>
-            <input type="text" />
-          </label>
-          <label>
-            <p>Password</p>
-            <input type="password" />
-          </label>
-          <div>
-            <button type="submit">Submit</button>
+        
+        <div className={`${toggleCardContainer}`}>
+          <div
+            className="app__Login-container-left"
+            style={{ visibility: btnState ? "visible" : "hidden" }}
+          >
+            <h1>Welcome</h1>
+            <p>Start modify your products here!</p>
+            <div className="input-block">
+              <label className="input-label">Email</label>
+              <input type="email" name="email" id="email" placeholder="Email" />
+            </div>
+            <div className="input-block">
+              <label className="input-label">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="Left-question-Login">
+              <a href="">forgot your password?</a>
+              <button>Login</button>
+            </div>
+            <div className="forgot-password">
+              <p>Don't have an account?</p>
+              <a href="">create account</a>
+            </div>
           </div>
-        </form>
-      </div> */}
-    </div>
+
+          <div className="app__Login-container-right">
+           <img src={images.funny} alt="" />
+          </div>
+
+          <input
+            id="app__Login-exit"
+            onClick={handleClick}
+            style={{ visibility: btnState ? "visible" : "hidden" }}
+            type="image"
+            src={images.close}
+          />
+        </div>
+        <button type="button" className="modal-button" onClick={handleClick}>
+          Click here to login
+        </button>
+      </div>
+    </>
   );
 }
 
---
+++ src/utils/images.ts
@@ -0,0 +1,7 @@
+import close from "../assets/close.png";
+import funny from "../assets/funny.jpg";
+
+export default {
+    close,
+    funny,
+};
